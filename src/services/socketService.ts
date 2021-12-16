cabd7f24897fbab4efec37ef85a9f077c282d993
Kateryna Vovk <vovk-k@bubuntu-lx.nixsolutions.com>
2021-12-16 19:43:34 +0200

    add user chat

--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -1,14 +1,80 @@
+/* eslint-disable camelcase */
 import { io } from 'socket.io-client';
+import { store as globalStore } from '@/store';
 
-class SocketioService {
+interface IMessage {
+  message: string;
+  type: number;
+  user: string;
+}
+interface IUser {
+  name: string;
+  user_id: string;
+  messages: any;
+}
+interface IUserInfo {
+  username: string;
+  type: string;
+}
+export class SocketioService {
   socket: any;
 
+  info: IUserInfo[] = [];
+
+  messages: IMessage[] = [];
+
+  username = null;
+
+  store = globalStore.state.auth;
+
+  constructor() {
+    this.info = [];
+    this.messages = [];
+    this.username = null;
+    this.store = globalStore.state.auth;
+    this.socket = io(`${process.env.VUE_APP_SOCKET_ENDPOINT}`, {
+      auth: { userName: this.store.userName, userId: this.store.userId },
+    });
+  }
+
   setupSocketConnection() {
-    console.warn('setupSocketConnection');
-    this.socket = io(`${process.env.VUE_APP_SOCKET_ENDPOINT}`, { auth: { userId: 'userId' } });
-    this.socket.emit('my message', 'Hello there from Vue.');
-    this.socket.on('my broadcast', (data: string) => {
-      console.log(data);
+    console.log('setupSocketConnection');
+    // this.socket = io(`${process.env.VUE_APP_SOCKET_ENDPOINT}`, {
+    //   auth: { userName: this.store.userName, userId: this.store.userId },
+    // });
+    this.addUser();
+
+    // this.socket.emit('my-message', 'Hello there from Vue.');
+
+    this.socket.on('chat-broadcast', (data: IMessage) => {
+      this.messages.push({
+        message: data.message,
+        type: 1,
+        user: data.user,
+      });
+      console.log('chat-broadcast', data);
+    });
+
+    this.socket.on('joined', (data: IUser) => {
+      this.info.push({
+        username: data.name,
+        type: 'joined',
+      });
+      this.messages.push(...data.messages);
+    });
+  }
+
+  send(input: any) {
+    console.warn('store', this.store, this.socket);
+    this.messages.push({
+      message: input,
+      type: 0,
+      user: 'Me',
+    });
+
+    this.socket.emit('my-message', {
+      message: input,
+      user: this.store.userName,
     });
   }
 
@@ -18,6 +84,16 @@ class SocketioService {
       this.socket.disconnect();
     }
   }
+
+  addUser() {
+    // this.ready = true;
+    console.warn('addUser', this.store.userName);
+    this.socket.emit('joined', this.store.userName);
+  }
+
+  // getMessages() {
+  //   return this.messages;
+  // }
 }
 
-export default new SocketioService();
+// export default new SocketioService();
--- src/views/UserChat.vue
+++ src/views/UserChat.vue
@@ -1,19 +1,55 @@
 <template lang="">
-  <div></div>
+  <div>
+    <ul>
+      <!-- <small v-if="typing" class="text-white">{{ typing }} is typing</small> -->
+      <li v-for="(message, i) in messages" :key="i">
+        <span>
+          <small>{{ message.user }}:</small>
+          {{ message.message }}
+        </span>
+      </li>
+    </ul>
+    <form @submit.prevent="send">
+      <div>
+        <input type="text" v-model="newMessage" placeholder="Enter message here" />
+      </div>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-import SocketioService from '@/services/socketService';
+import { defineComponent } from 'vue';
+import { SocketioService } from '@/services/socketService';
 
-export default {
+// const newSocket = new SocketioService();
+
+export default defineComponent({
   name: 'UserChat',
+  data() {
+    return {
+      newSocket: new SocketioService(),
+      messages: [],
+      newMessage: null,
+    };
+  },
+  methods: {
+    send() {
+      this.newSocket.send(this.newMessage);
+      this.newMessage = null;
+    },
+  },
   created() {
-    SocketioService.setupSocketConnection();
+    this.newSocket.setupSocketConnection();
+    // this.newSocket.addUser();
+    //     watchEffect(() => {
+    //       this.messages = newSocket.getMessages();
+    // },
   },
+
   beforeUnmount() {
-    SocketioService.disconnect();
+    this.newSocket.disconnect();
   },
-};
+});
 </script>
 
 <style lang=""></style>
